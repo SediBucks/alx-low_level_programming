c2260baa9fdd19987c5eb619ccf90610cb58b237
Lesedi <michellelesedi@gmail.com>
2023-03-21 13:24:44 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,7 @@
-#include "main.h"
-int _islower(int c)
-{
-	if (c> 'a' && c < 'z'
-			{return (1);
-			}
-		else 
-		{
-		return (0);
-		}
-	}
+int _islower(int c) {
+    if (c >= 'a' && c <= 'z') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
