f110a00e73f9d3d6dc3d57a915ac8fdd0bb506e1
SediBucks <michellelesedi@gmail.com>
2023-03-27 13:51:00 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -password.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+int i, rand_num;
+srand(time(NULL));
+
+ for (i = 0; i < PASSWORD_LENGTH; i++) {
+        rand_num = rand() % 62; 
+        if (rand_num < 26) {
+            password[i] = 'a' + rand_num;         } else if (rand_num < 52) {
+            password[i] = 'A' + rand_num - 26;s
+        } else {
+            password[i] = '0' + rand_num - 52;
+        }
+    }
+    password[PASSWORD_LENGTH] = '\0';
+    
+    // print the password
+    printf("%s\n", password);
+    
+    return 0;
