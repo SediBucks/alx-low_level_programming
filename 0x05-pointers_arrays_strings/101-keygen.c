d83d79f1137ac6552101935634d7f7c96ed957bf
SediBucks <michellelesedi@gmail.com>
2023-03-27 15:10:38 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define PASSWORD_LENGTH 100
 #define MAX_ASCII_CODE 77
+#define EXPECTED_PASSWORD "Tada! Congrats"
 
 int main(void)
 {
     int password[PASSWORD_LENGTH];
     int i, sum, remaining;
+    char password_str[PASSWORD_LENGTH + 1];
 
     sum = 0;
     srand(time(NULL));
@@ -22,6 +25,18 @@ int main(void)
             putchar(remaining + '0');
             break;
         }
+    }
+
+    password_str[i + 1] = '\0';
+
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        password_str[i] = password[i] + '0';
+    }
+
+    if (strcmp(password_str, EXPECTED_PASSWORD) == 0) {
+        printf("\nCongratulations! The password is correct.\n");
+    } else {
+        printf("\nSorry, the password is incorrect.\n");
     }
 
     return 0;
