d5fa000d31ac44d7a80a57dcfc325b2e52a75c0e
SediBucks <michellelesedi@gmail.com>
2023-03-27 12:40:17 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line,
+ * @str: value
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
