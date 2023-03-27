8e2d881826b6ed666a34d3692abb961cc596c7fe
SediBucks <michellelesedi@gmail.com>
2023-03-27 15:03:01 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
-#define PASSWORD_LENGTH 15
+#define PASSWORD_LENGTH 100
+#define MAX_ASCII_CODE 77
 
 int main(void)
 {
-    int i;
-    char password[PASSWORD_LENGTH + 1];
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int password[PASSWORD_LENGTH];
+    int i, sum, remaining;
 
+    sum = 0;
     srand(time(NULL));
 
     for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = charset[rand() % (sizeof(charset) - 1)];
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-
-    printf("Generated Password: %s\n", password);
-
-    if (strcmp(password, "Tada! Congrats") == 0) {
-        printf("Success!\n");
-    } else {
-        printf("Wrong password\n");
+        password[i] = rand() % MAX_ASCII_CODE;
+        sum += password[i];
+        putchar(password[i] + '0');
+        if (sum >= 2772) {
+            remaining = 2772 - sum + password[i];
+            putchar(remaining + '0');
+            break;
+        }
     }
 
     return 0;
