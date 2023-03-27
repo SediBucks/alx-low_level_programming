98233d63d60618ce3c36439c6812d2ec5d693f47
SediBucks <michellelesedi@gmail.com>
2023-03-27 15:27:51 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,44 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
-#define PASSWORD_LENGTH 100
-#define MAX_ASCII_CODE 77
-#define EXPECTED_PASSWORD "Tada! Congrats"
+#define LENGTH 8 // password length
+#define CHARS 
+"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};':\"\\|,.<>/?`~" 
 
-int main(void)
-{
-    int password[PASSWORD_LENGTH];
-    int i, sum, remaining;
-    char password_str[PASSWORD_LENGTH + 1];
+/**
+ * main -password.
+ * Return: Always 0.
+ */
 
-    sum = 0;
-    srand(time(NULL));
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = rand() % MAX_ASCII_CODE;
-        sum += password[i];
-        putchar(password[i] + '0');
-        if (sum >= 2772) {
-            remaining = 2772 - sum + password[i];
-            putchar(remaining + '0');
-            break;
-        }
+int main() {
+    srand(time(NULL)); 
+
+    char password[LENGTH + 1];
+
+    for (int i = 0; i < LENGTH; i++) {
+        password[i] = CHARS[rand() % (sizeof(CHARS) - 1)];
     }
 
-    password_str[i + 1] = '\0';
+    password[LENGTH] = '\0';
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password_str[i] = password[i] + '0';
-    }
-
-    if (strcmp(password_str, EXPECTED_PASSWORD) == 0) {
-        printf("\nCongratulations! The password is correct.\n");
-    } else {
-        printf("\nSorry, the password is incorrect.\n");
-    }
-
+    printf("Password: %s\n", password);
     return 0;
 }
-
