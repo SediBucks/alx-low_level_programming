d99aaef0f2ff3484d0e59db5661c12ed008261e3
SediBucks <michellelesedi@gmail.com>
2023-03-27 14:03:35 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,22 +7,20 @@
  * Return: Always 0.
  */
 
-#define PASSWORD_LENGTH 12
-
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];  // add one to include null terminator
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=";
+    char password[9]; 
+    const char charset[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+    srand(time(NULL)); 
 
-    srand(time(NULL));
-
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = charset[rand() % sizeof(charset)];
+    for (int i = 0; i < 8; i++) {
+        password[i] = charset[rand() % 36];
     }
 
-    password[PASSWORD_LENGTH] = '\0';
+    password[8] = '\0'; 
 
     printf("Random Password: %s\n", password);
 
     return 0;
 }
+
