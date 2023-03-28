71a16520f598c308c2c4f428d7954fd6ff2e367e
SediBucks <michellelesedi@gmail.com>
2023-03-27 19:41:25 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,53 +7,29 @@
  * Return: Always 0.
  */
 
-int main() {
-
-char numbers[] = "0123456789";
-char letter[] = "abcdefghijklmnoqprstuvwyzx";
-char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-char symbols[] = "!@#$^&*?";
-
-int size;
-scanf("%d",&size);
-
-char password[20];
-
-srand((unsigned int)(time(NULL)));
-
-for(int i=0; i<size; i++ )
+int main(void)
 {
-    int random_number = rand()%4;
+	int pass[100];
+	int i, sum, n;
 
-    if(random_number == 0)
-    {
+	sum = 0;	
 
-random_number = rand()%10;
+	srand(time(NULL));
 
-password[i] = numbers[random_number];
-    }
-    else if(random_number == 1)
-    {
-random_number = rand()%26;
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
-        password[i] = letter[random_number];
-    }
-    else if(random_number == 2)
-    {
-random_number = rand()%26;
-
-        password[i] = LETTER[random_number];
-    }
-    else
-    {
- random_number = rand()%8;
-
-        password[i] = symbols[random_number];
-    }
+	return (0);
 }
-for(int i=0;i<size;i++)
-    {
-      printf("%c", password[i]);
-    }
-  return 0;
-}
+
