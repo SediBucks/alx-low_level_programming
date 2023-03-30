1ade245a4e12ce595859abf7e743a37cd7a54b15
SediBucks <michellelesedi@gmail.com>
2023-03-30 15:20:54 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * reverse_array - reverses the content of an array
  * of integers.
@@ -6,20 +7,14 @@
  * @n: value.
  * Return: Always0..
  */
-int _strcmp(char *s1, char *s2)
-{
-	int v1 = 0, v2 = 0;
 
-	for (; *(s1 + v1) != '\0'; v1++)
-		;
-	--v1;
-	for (; *(s2 + v2) != '\0'; v2++)
-		;
-	--v2;
-	if (v1 < v2)
-		return (-v1);
-	else if (v1 > v2)
-		return (+v1);
-	else
-		return (0);
+int _strcmp (char *str1, char *str2) 
+{
+    while (*str1 != '\0' && *str1 == *str2) 
+{
+        str1++;
+        str2++;
+    }
+    return (*str1 - *str2);
 }
+
