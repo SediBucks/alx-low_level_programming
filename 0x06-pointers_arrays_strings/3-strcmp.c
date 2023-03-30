6be53053abdcdf11a0a0b2ba6af6fac68214346e
SediBucks <michellelesedi@gmail.com>
2023-03-30 15:08:31 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,17 +5,21 @@
  * @s2: second string.
  * Return: Always 0. 
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int l1 = 0, l2 = 0;
 
-	while (op == 0)
-	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
-	}
-
-	return (op);
+	for (; *(s1 + l1) != '\0'; l1++)
+		;
+	--l1;
+	for (; *(s2 + l2) != '\0'; l2++)
+		;
+	--l2;
+	if (vl1 < l2)
+		return (-l1);
+	else if (l1 > l2)
+		return (+l1);
+	else
+		return (0);
 }
