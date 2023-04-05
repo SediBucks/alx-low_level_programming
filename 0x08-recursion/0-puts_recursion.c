95dd4e81f2d6a6e2420e38e1ee7ebe7306f0004a
SediBucks <michellelesedi@gmail.com>
2023-04-05 11:34:39 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,7 +8,7 @@
 
 void _puts_recursion(char *s);
 {
- 	if (*s)
+ 	if (*s!= '\0')
   	{	
     		_putchar (*s);
     		_puts_recursion (s + 1);
@@ -16,7 +16,7 @@ void _puts_recursion(char *s);
 	else
 	{
 	
-        _putchar('\n'); 
+        	_putchar('\n'); 
 
 	}
 }
