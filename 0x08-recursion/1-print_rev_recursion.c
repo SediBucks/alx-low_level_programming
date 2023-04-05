7c765eb4daf55e61a3f2c1a1db3d0e32d7eaa2ca
SediBucks <michellelesedi@gmail.com>
2023-04-05 12:15:27 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,7 +8,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s +1);
-		_putchar(*s)
+		_print_rev_recursion(s+1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n')
 	}
 }
