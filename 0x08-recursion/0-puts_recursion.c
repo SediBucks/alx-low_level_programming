0a412fb201122a4afce9946a5cd0053e6baf5a60
SediBucks <michellelesedi@gmail.com>
2023-04-05 11:57:09 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,18 +3,17 @@
 /**
  * _puts_recursion - prints a string
  * @s: pointer to string
- *
- * Return: null
+ * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
-        {
+	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-        }
+	}
 	else
-        {
-                _putchar('\n');
-        }
+	{
+		_putchar('\n');
+	}
 }
