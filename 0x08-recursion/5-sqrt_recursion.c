db5f6e3a27fdbf09d399b36cdf160c9bbca8b3a2
SediBucks <michellelesedi@gmail.com>
2023-04-06 15:18:24 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _sedi - finds root
+ * @x: value
+ * @y: value
+ * Return: Always 0.
+ */
+	int _sedi(int x, int y)
+{
+		if (y * y == x)
+			return (y);
+		if (y * y < x)
+			return (_sedi(x, ++y));
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: value
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+			return (_sedi(1,n));
+}
