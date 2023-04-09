f73448b804b4176d8270b4e09da64ca4d6126874
SediBucks <michellelesedi@gmail.com>
2023-04-09 09:46:54 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,11 +8,23 @@
 
 int is_prime_number(int n)
 {
-	if (n < 2)
-		return 0;
+	if (n <= 1)
+		return (0);
+		return (sedi(n, 2));
+}
 
-	for (int i=2; i <= n / 2; i++)
-		if (n % i == 0)
-			return 0;
-	return 1
+/**
+ * sedi - returns 1 if the input integer is a prime number, otherwise return 0.
+ * @x: value
+ * @y: variable
+ * Return: Always 0.
+ */
+
+{
+	int sedi(int x, int y)
+			if (x == y)
+		return (1);
+	if (x % y == 0)
+		return (0);
+	return (sedi(x, y + 1));
 }
